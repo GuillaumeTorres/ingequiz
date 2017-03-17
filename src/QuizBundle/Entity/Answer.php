c03e1def14135ae122add81506328ada647b9294
guillaume <guillaume.torres91@gmail.com>
2017-03-17 14:04:30 +0100

    Mise en place de la base de donnée

---
+++ src/QuizBundle/Entity/Answer.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace QuizBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Answer
+ *
+ * @ORM\Table(name="answer")
+ * @ORM\Entity(repositoryClass="QuizBundle\Repository\AnswerRepository")
+ */
+class Answer
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="answer", type="string", length=255)
+     */
+    private $answer;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="is_right", type="boolean")
+     */
+    private $isRight;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set answer
+     *
+     * @param string $answer
+     *
+     * @return Answer
+     */
+    public function setAnswer($answer)
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    /**
+     * Get answer
+     *
+     * @return string
+     */
+    public function getAnswer()
+    {
+        return $this->answer;
+    }
+
+    /**
+     * Set isRight
+     *
+     * @param boolean $isRight
+     *
+     * @return Answer
+     */
+    public function setIsRight($isRight)
+    {
+        $this->isRight = $isRight;
+
+        return $this;
+    }
+
+    /**
+     * Get isRight
+     *
+     * @return bool
+     */
+    public function getIsRight()
+    {
+        return $this->isRight;
+    }
+}
+
---
+++ src/QuizBundle/Entity/Score.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace QuizBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Score
+ *
+ * @ORM\Table(name="score")
+ * @ORM\Entity(repositoryClass="QuizBundle\Repository\ScoreRepository")
+ */
+class Score
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="score", type="float", nullable=true)
+     */
+    private $score;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set score
+     *
+     * @param float $score
+     *
+     * @return Score
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    /**
+     * Get score
+     *
+     * @return float
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+}
+
