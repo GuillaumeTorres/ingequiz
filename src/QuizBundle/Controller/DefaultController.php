a6fa8fb87fe055b8f5fbafe6278e3563696826d8
guillaume <guillaume.torres91@gmail.com>
2017-03-24 11:50:09 +0100

    Mise en place génération du questionnaire

--- src/QuizBundle/Controller/DefaultController.php
+++ src/QuizBundle/Controller/DefaultController.php
@@ -2,9 +2,16 @@
 
 namespace QuizBundle\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use QuizBundle\Entity\Answer;
+use QuizBundle\Entity\Question;
+use QuizBundle\Entity\Quiz;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -15,7 +22,7 @@ class DefaultController extends Controller
         $form = $this->createFormBuilder()
           ->add('quiz' , EntityType::class, array(
             'class' => 'QuizBundle\Entity\Quiz',
-            'label' => 'name',
+            'label' => 'Questionnaire',
             'attr' => array('class' => 'form-group'),
           ))
           ->add('start_tournament', SubmitType::class, array(
@@ -30,8 +37,56 @@ class DefaultController extends Controller
       $data = $request->request->all();
       $quizId = $data['form']['quiz'];
 
+      /** @var Quiz $quiz */
       $quiz = $this->container->get('doctrine')->getManager()->getRepository('QuizBundle:Quiz')->findOneBy(array('id' => $quizId));
+      $form = $this->generateForm($quiz);
 
-      return $this->render('QuizBundle:Default:quiz.html.twig', array('quiz' => $quiz));
+      return $this->render('QuizBundle:Default:quiz.html.twig', array(
+        'form' => $form->createView(),
+        'title' => $quiz->getName(),
+        ));
     }
+
+  /**
+   * @param Quiz $quiz
+   *
+   * @return Form
+   */
+  private function generateForm($quiz)
+  {
+    $form = $this->createFormBuilder();
+    $questions = $quiz->getQuestion();
+    /** @var Question $question */
+    foreach ($questions as $key => $question) {
+      $answers = $this->formatAnswer($question->getAnswer());
+      $form->add('question'.$key, ChoiceType::class, array(
+        'choices' => $answers,
+        'multiple' => true,
+        'label' => $question->getQuestion(),
+      ));
+    }
+    $form
+      ->add('save', SubmitType::class, array(
+      'attr' => array('class' => 'btn btn-primary'),
+      'label' => 'Obtenir son résultat',
+    ));
+
+    return $form->getForm();
+  }
+
+  /**
+   * @param ArrayCollection $answers
+   *
+   * @return array
+   */
+  private function formatAnswer($answers)
+  {
+    $formatAnswers = [];
+    /** @var Answer $answer */
+    foreach ($answers as $answer) {
+      $formatAnswers[$answer->getAnswer()] = $answer->getIsRight();
+    }
+
+    return $formatAnswers;
+  }
 }
